a2ae415dbb24a4e8bf6369fcca805932d7be4e64
Sugumar <sugumarb@b2b.regn.net>
2023-07-21 11:51:11 +0100

    model summarisation

---
+++ text_generation/openai.py
@@ -0,0 +1,9 @@
+from transformers import OpenAIGPTTokenizer, TFOpenAIGPTModel
+
+tokenizer = OpenAIGPTTokenizer.from_pretrained("openai-gpt")
+model = TFOpenAIGPTModel.from_pretrained("openai-gpt")
+
+inputs = tokenizer("Hello, my dog is cute", return_tensors="tf")
+outputs = model(inputs)
+
+last_hidden_states = outputs.last_hidden_state
